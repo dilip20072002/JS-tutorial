6ed3087b8e1d05d1c6245e339c3670103bb19219
Dilip Patel <95010587+dilip20072002@users.noreply.github.com>
2025-07-04 09:59:05 +0000

    Container config done

---
+++ 1_basics/2_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(2+3) // we are using node js, not browser
+
+console.log(2+3);
+
+let name = "Dilip"
+let age = 24
+let isLoggedIn = true
+let state = null
+
+/*
+number => 2 to the power 53
+bigint =>
+string => " "
+boolean => true/false
+null => standalone value
+undefined => (let state;)
+symbol => unique
+*/
+
+// object
+console.log(typeof null); // onject
+console.log(typeof undefined); // object
