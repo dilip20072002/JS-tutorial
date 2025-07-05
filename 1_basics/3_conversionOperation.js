0dbd227317e73c4ec1012ac6c8d54c8ef9ff9193
Dilip Patel <95010587+dilip20072002@users.noreply.github.com>
2025-07-05 16:52:13 +0000

    Conversion of Data types

---
+++ 1_basics/3_conversionOperation.js
@@ -0,0 +1,30 @@
+let score1 = "33abc"
+let score2 = null
+let score3 = undefined
+
+console.log(typeof score1);
+console.log(typeof score2);
+ 
+
+let valueNumber1 = Number(score1)
+let valueNumber2 = Number(score2)
+
+console.log(typeof valueNumber1);
+console.log(valueNumber1);
+
+console.log(typeof valueNumber2);
+console.log(valueNumber2);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
+let isLoggedIn = "dilip"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// let isLoggedIn = ""
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
