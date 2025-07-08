97b4c306b43de86ccf69c6dcd0e43e82a59f3397
Dilip Patel <95010587+dilip20072002@users.noreply.github.com>
2025-07-08 07:29:16 +0000

    IIFE Done

---
+++ 3_basics/4_iife.js
@@ -0,0 +1,9 @@
+// IIFE definition - ()()
+// function chai(){                   // Basic function calling
+//     console.log(`Dilip`);
+// }
+// chai ()
+
+(function chai(){                    // Calling function using IIFE
+    console.log(`Dilip`);
+}) ()
