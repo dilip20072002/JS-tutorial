2d78e0dfbad82153cd798af894d3b745a1598a91
Dilip Patel <95010587+dilip20072002@users.noreply.github.com>
2025-07-08 07:35:39 +0000

    IIFE done

--- 3_basics/4_iife.js
+++ 3_basics/4_iife.js
@@ -6,4 +6,12 @@
 
 (function chai(){                    // Calling function using IIFE
     console.log(`Dilip`);
-}) ()
+}) ();                              // If we are using two or more iife function together, then have to use ';' after every funtion
+
+(function chaiCode(){                    // Calling function using IIFE
+    console.log(`Dilip Patel`);
+}) ();
+
+(function chaiHai(price){           // Calling function using IIFE with parameter
+    console.log(`My book price is ${price}`);
+}) (5)
