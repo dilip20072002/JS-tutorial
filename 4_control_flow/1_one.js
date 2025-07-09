a5be5e0d8cf73c45c7c3115bb1b05819c3700d2f
Dilip Patel <95010587+dilip20072002@users.noreply.github.com>
2025-07-09 16:29:01 +0000

    If, else if and else statement

---
+++ 4_control_flow/1_one.js
@@ -0,0 +1,18 @@
+// If, Else, and Else if statement - 
+
+if(true){
+    // console.log("Execute");  
+}
+else{
+    // console.log("Not Execute");
+}
+
+// <, >, =, <=, >=, ==, !=, ===, !==    (sign '===' and '!==' indicate the caparision of both value and data type together)
+// For example - 
+if(2=="2"){
+    // console.log("True");
+}
+if(2==="2"){
+    // console.log("True");
+}
+
