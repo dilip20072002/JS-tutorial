83fc91044b303de0b9dd56ecbb74f29ecca09e28
Dilip Patel <95010587+dilip20072002@users.noreply.github.com>
2025-07-10 16:27:00 +0000

    Switch statement

---
+++ 4_control_flow/2_switch.js
@@ -0,0 +1,31 @@
+// *** Syntex of switch statement - 
+
+// switch (key) {
+//     case value:     
+//         break;
+//     default:
+//         break;
+// }
+
+
+const month = 6
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    default:
+        console.log("Not Applicable");
+        break;
+}
