d5a6b687a49348c9703e2ebc1d074f7005d8c889
Dilip Patel <95010587+dilip20072002@users.noreply.github.com>
2025-07-11 11:14:08 +0000

    While and do while

---
+++ 5_iteration/2_while_doWhile.js
@@ -0,0 +1,14 @@
+//while loop -
+let i = 0;
+while(i<5){
+    // console.log(`Value of i is ${i}`);
+    i++;
+}
+
+
+// do while loop -
+let j = 11;
+do {
+    console.log(`Value of j is ${j}`);
+    j++; 
+} while (j<10);
