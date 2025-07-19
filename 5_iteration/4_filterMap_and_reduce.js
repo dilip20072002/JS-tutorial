eb7ae33088026aba5eeeda21accd80298a333db3
Dilip Patel <95010587+dilip20072002@users.noreply.github.com>
2025-07-19 05:28:14 +0000

    Filter Map and Reduce

---
+++ 5_iteration/4_filterMap_and_reduce.js
@@ -0,0 +1,17 @@
+const arr = ["a", "b", "c", "d", "e"]
+const value = arr.forEach((num) => {
+    // console.log(num)
+    return num               // forEach doesn't retrun the value
+})
+// console.log(value)
+
+const myNum = [1,2,3,4,5,6,7,8,9]
+const value1 = myNum.filter((num) => num > 4)     // 1st way to call filter without use '{}'
+// console.log(value1);
+
+const value2 = myNum.filter((num) => {           // 2nd way to call filter with use '{}'
+// console.log(value1);
+    return (num > 4)
+})
+// console.log(value2);
+
